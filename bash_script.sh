b4c89486c07d361fcd8f0beb3975656e9120e471
amandadumit <amandadumit@gmail.com>
2020-10-01 22:34:13 +0000

    adicionando arquivos da questão 2 da prova

---
+++ bash_script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# um comando que mostre a pasta/diretório em que você está:
+
+pwd
+
+# um comando que gere uma lista com detalhes do conteúdo da sua pasta HOME e redirecione a pasta para um arquivo chamado "lista_HOME.txt"
+
+ls -l /home/adum > lista_HOME.txt
+
+# um comando que imprima a data atual na tela
+
+date
+
+
+
+
+
+
