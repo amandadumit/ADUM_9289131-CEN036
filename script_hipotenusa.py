5bb6dbf15c723f0c10e8b93c8d774b7b70c274de
amandadumit <amandadumit@gmail.com>
2020-10-02 16:34:25 +0000

    consertando scripts lista_home e o da hipotenusa

--- script_hipotenusa.py
+++ script_hipotenusa.py
@@ -4,7 +4,7 @@
 
 import sys
 
-#aqui vamos testar se o valor de a e b são inteiros, usando a função isinstance () e imprimir uma mensagem de confirmação ou de erro
+#aqui vamos testar se o valor de a e b são inteiros, usando a função isinstance () e se eles estão entre 0 e 1000,  e imprimir uma mensagem de confirmação ou de erro
 
 verify_a = isinstance(int(sys.argv[1]), int)
 
@@ -12,15 +12,13 @@ verify_b = isinstance(int(sys.argv[2]), int)
 
 if (verify_a == True and verify_b == True):
 	print("os valores de a e b são inteiros")
+	if (0 < int(sys.argv[1]) < 1000) and (0 < int(sys.argv[2]) < 1000): 
+		print("os valores de a e b são inteiros e estão entre 0 e 1000")
+	else:
+		print("os valores de a e/ou b são inteiros mas não estão entre 0 e 1000") 
 else:
 	print("os valores de a e/ou b não são inteiros")
 
-# aqui vamos testar se eles são inteiros positivos e menores que 1000 e imprimir mensagens de confirmação ou de erro
-
-if (0 < int(sys.argv[1]) < 1000) and (0 < int(sys.argv[2]) < 1000):
-	print("os valores de a e b são inteiros e estão entre 0 e 1000")
-else:
-	print("os valores de a e/ou b são inteiros mas  não estão entre 0 e 1000")
 
 # aqui vamos calcular o quadrado da hipotenusa, que é dado pela fórmula matemática: hip^2 = a^2 + b^2 
 
